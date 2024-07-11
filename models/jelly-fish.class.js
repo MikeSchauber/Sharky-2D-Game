739a0de3eb4aa4585131c16daff1719a6a111f58
MikeSchauber <mike.schauber96@gmail.com>
2024-07-11 22:19:33 +0200

    add shark animation with modulu operator arrays and loop

--- models/jelly-fish.class.js
+++ models/jelly-fish.class.js
@@ -1,6 +1,7 @@
 class JellyFish extends MovableObject {
     width = 65;
     height = 80;
+    pixel = 0.5
     constructor() {
         super().loadImage("img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png")
         this.x = 300 + Math.random() * 375
@@ -9,16 +10,16 @@ class JellyFish extends MovableObject {
     }
 
     animate() {
-        if (this.y = 360) {
-            setInterval(() => {
-                this.y -= 1
-            }, 1000 / 60)
-        }
-        if (this.y = 0) {
-            setInterval(() => {
-                this.y += 1
-            }, 1000 / 60)
-        }
+        setInterval(() => {
+            this.animateUp();
+        }, 1000 / this.fps)
+    }
 
+    animateUp() {
+        this.y -= this.pixel
+    }
+
+    animateDown() {
+        this.y += this.pixel
     }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,26 @@
 class MovableObject{
+    fps = 60;
+    pixel = 1;
     img;
+    imageCache = {};
     width = 720;
     height = 480;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr = ["img/image1.png", "img/image2.png", ...];
+     */
+    loadImages(arr) {
+        arr.forEach((src) => {
+            let img = new Image();
+            img.src = src
+            this.imageCache[src] = img;
+        })
     }
 
     moveRight() {   
