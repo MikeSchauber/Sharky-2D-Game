f9b0f39896e07d3bcf4c88861a5e6df00b5e5e45
Mike Schauber <mike.schauber96@gmail.com>
2024-07-23 11:34:04 +0200

    check world end for throwable objects

--- models/world.class.js
+++ models/world.class.js
@@ -47,186 +47,194 @@ class World {
     run() {
         setInterval(() => {
             this.checkCollisions();
-            // this.checkThrowObjects();
+            this.checkThrowableObjectsRange();
         }, 50);
     }
 
-    checkCollisions() {
-        this.checkCharacterDamage();
-        this.checkCharacterCoins();
-        this.checkCharacterPoison();
-        this.checkThrowableObjects();
-    }
-
-    checkCharacterDamage() {
-        this.level.enemies.forEach(enemy => {
-            if (this.character.isColliding(enemy) && !this.character.attacking && !this.character.isDead()) {
-                this.character.hit();
-                this.character.damagedBy = enemy.damage;
-            } else if (!this.character.isColliding(enemy)) {
-                this.character.isntHit();
-            }
-        });
-    }
-
-    checkCharacterCoins() {
-        this.level.coins.forEach((coin, i) => {
-            if (this.character.isColliding(coin)) {
-                if (!coin.collected) {
-                    this.character.collectCoin();
-                    this.coin_sound.play();
-                    coin.collected = true;
-                    this.bars[2].setPercentage(this.bars[2].IMAGES_COIN, this.character.coins);
-                    this.level.coins.splice(i, 1);
-                }
-            }
-        });
-    }
-
-    checkCharacterPoison() {
-        this.level.poison.forEach((poison, i) => {
-            if (this.character.isColliding(poison)) {
-                if (!poison.collected) {
-                    this.character.collectPoison();
-                    this.poison_collect_sound.play();
-                    poison.collected = true;
-                    this.bars[1].setPercentage(this.bars[1].IMAGES_POISON, this.character.poison);
-                    this.level.poison.splice(i, 1);
-                }
-            }
-        });
-    }
-
-    checkThrowableObjects() {
-        if (this.throwableObjects.length >= 1) {
-            this.throwableObjects.forEach(throwableObject => {
-                if (throwableObject.isColliding(this.level.enemies)) {
-                    console.log("hitting enemy", enemy);
-                }
+    checkThrowableObjectsRange() {
+        if (this.throwableObjects.length > 0) {
+            this.throwableObjects.forEach((object, i) => {
+                if (object.x >= this.level.level_end_x + 350) {
+                    this.throwableObjects.splice(i, 1);
+               }
             });
-        }
     }
+}
 
-    setWorld() {
-        this.character.world = this;
-        this.level.world = this;
-        this.bars.forEach((bar) => {
-            bar.world = this;
+checkCollisions() {
+    this.checkCharacterDamage();
+    this.checkCharacterCoins();
+    this.checkCharacterPoison();
+    this.checkThrowableObjects();
+}
+
+checkCharacterDamage() {
+    this.level.enemies.forEach(enemy => {
+        if (this.character.isColliding(enemy) && !this.character.attacking && !this.character.isDead()) {
+            this.character.hit();
+            this.character.damagedBy = enemy.damage;
+        } else if (!this.character.isColliding(enemy)) {
+            this.character.isntHit();
+        }
+    });
+}
+
+checkCharacterCoins() {
+    this.level.coins.forEach((coin, i) => {
+        if (this.character.isColliding(coin)) {
+            if (!coin.collected) {
+                this.character.collectCoin();
+                this.coin_sound.play();
+                coin.collected = true;
+                this.bars[2].setPercentage(this.bars[2].IMAGES_COIN, this.character.coins);
+                this.level.coins.splice(i, 1);
+            }
+        }
+    });
+}
+
+checkCharacterPoison() {
+    this.level.poison.forEach((poison, i) => {
+        if (this.character.isColliding(poison)) {
+            if (!poison.collected) {
+                this.character.collectPoison();
+                this.poison_collect_sound.play();
+                poison.collected = true;
+                this.bars[1].setPercentage(this.bars[1].IMAGES_POISON, this.character.poison);
+                this.level.poison.splice(i, 1);
+            }
+        }
+    });
+}
+
+checkThrowableObjects() {
+    if (this.throwableObjects.length >= 1) {
+        this.level.enemies.forEach((enemy, i) => {
+
         });
-    };
-
-    setSounds() {
-        this.soundtrack = new Audio("audio/Shark game song.mp3");
-        this.ambient_sound = new Audio("audio/ambient.mp3");
-        this.coin_sound = new Audio("audio/coin.mp3");
-        this.poison_collect_sound = new Audio("audio/poison.mp3");
-        this.bubble_shot = new Audio("audio/bubble-shot.mp3");
-        this.poison_bubbleshot_sound = new Audio("audio/poison-bubble.mp3");
-        this.walking_sound = new Audio("audio/swim Sound.mp3");
-        this.electro_hitsound = new Audio("audio/electro-damage.mp3");
-        this.error_sound = new Audio("audio/error.mp3");
-        this.ouch_sound = new Audio("audio/ouch.mp3");
-        this.electrodeath_sound = new Audio("audio/bones.mp3");
-        this.snoring_sound = new Audio("audio/snoring.mp3");
     }
+}
 
-    setEffectVolume() {
-        this.soundtrack.volume = this.musicVolume;
-        this.ambient_sound.volume = this.musicVolume;
-        this.coin_sound.volume = this.effectVolume;
-        this.walking_sound.volume = this.effectVolume;
-        this.electro_hitsound.volume = this.effectVolume;
-        this.bubble_shot.volume = this.effectVolume;
-        this.poison_bubbleshot_sound.volume = this.effectVolume;
-        this.ouch_sound.volume = this.effectVolume;
-        this.electrodeath_sound.volume = this.effectVolume;
-        this.snoring_sound.volume = this.effectVolume;
-        this.error_sound.volume = this.effectVolume - 0.2;
-    };
+setWorld() {
+    this.character.world = this;
+    this.level.world = this;
+    this.bars.forEach((bar) => {
+        bar.world = this;
+    });
+};
 
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.translate(this.camera_x, 0);
-        this.firstBackground();
-        this.lights();
-        this.middleBackground();
-        this.frontObjects();
-        this.fixedObjects();
-        this.ctx.translate(-this.camera_x, 0);
-        let self = this;
-        requestAnimationFrame(function () {
-            self.draw();
-        });
+setSounds() {
+    this.soundtrack = new Audio("audio/Shark game song.mp3");
+    this.ambient_sound = new Audio("audio/ambient.mp3");
+    this.coin_sound = new Audio("audio/coin.mp3");
+    this.poison_collect_sound = new Audio("audio/poison.mp3");
+    this.bubble_shot = new Audio("audio/bubble-shot.mp3");
+    this.poison_bubbleshot_sound = new Audio("audio/poison-bubble.mp3");
+    this.walking_sound = new Audio("audio/swim Sound.mp3");
+    this.electro_hitsound = new Audio("audio/electro-damage.mp3");
+    this.error_sound = new Audio("audio/error.mp3");
+    this.ouch_sound = new Audio("audio/ouch.mp3");
+    this.electrodeath_sound = new Audio("audio/bones.mp3");
+    this.snoring_sound = new Audio("audio/snoring.mp3");
+}
+
+setEffectVolume() {
+    this.soundtrack.volume = this.musicVolume;
+    this.ambient_sound.volume = this.musicVolume;
+    this.coin_sound.volume = this.effectVolume;
+    this.walking_sound.volume = this.effectVolume;
+    this.electro_hitsound.volume = this.effectVolume;
+    this.bubble_shot.volume = this.effectVolume;
+    this.poison_bubbleshot_sound.volume = this.effectVolume;
+    this.ouch_sound.volume = this.effectVolume;
+    this.electrodeath_sound.volume = this.effectVolume;
+    this.snoring_sound.volume = this.effectVolume;
+    this.error_sound.volume = this.effectVolume - 0.2;
+};
+
+draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.translate(this.camera_x, 0);
+    this.firstBackground();
+    this.lights();
+    this.middleBackground();
+    this.frontObjects();
+    this.fixedObjects();
+    this.ctx.translate(-this.camera_x, 0);
+    let self = this;
+    requestAnimationFrame(function () {
+        self.draw();
+    });
+}
+
+firstBackground() {
+    this.ctx.translate(-this.camera_x * 0.8, 0);
+    this.addObjectsToMap(this.level.backgroundObjects);
+    this.ctx.translate(this.camera_x * 0.8, 0);
+}
+
+lights() {
+    this.ctx.translate(-this.camera_x * 0.9, 0);
+    this.addObjectsToMap(this.level.lights);
+    this.ctx.translate(this.camera_x * 0.9, 0);
+}
+
+middleBackground() {
+    this.ctx.translate(-this.camera_x * 0.3, 0);
+    this.addObjectsToMap(this.level.middlegroundObjects);
+    this.ctx.translate(this.camera_x * 0.3, 0);
+}
+
+fixedObjects() {
+    this.ctx.translate(-this.camera_x, 0);
+    this.addObjectsToMap(this.bars);
+    this.ctx.translate(this.camera_x, 0);
+}
+
+frontObjects() {
+    this.addObjectsToMap(this.level.frontgroundObjects);
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.throwableObjects);
+    this.addObjectsToMap(this.level.coins);
+    this.addObjectsToMap(this.level.poison);
+    this.addToMap(this.character);
+}
+
+addObjectsToMap(objects) {
+    objects.forEach(object => {
+        this.addToMap(object);
+    })
+}
+
+addToMap(mo) {
+    this.ctx.save();
+    if (mo.leftDirection) {
+        this.flipImage(mo);
     }
-
-    firstBackground() {
-        this.ctx.translate(-this.camera_x * 0.8, 0);
-        this.addObjectsToMap(this.level.backgroundObjects);
-        this.ctx.translate(this.camera_x * 0.8, 0);
+    if (mo.upperDirection) {
+        this.rotateUpwards(mo);
     }
-
-    lights() {
-        this.ctx.translate(-this.camera_x * 0.9, 0);
-        this.addObjectsToMap(this.level.lights);
-        this.ctx.translate(this.camera_x * 0.9, 0);
+    mo.draw(this.ctx, mo);
+    mo.drawBorder(this.ctx);
+    if (mo.leftDirection) {
+        this.flipImageBack(mo);
     }
+    this.ctx.restore();
+}
 
-    middleBackground() {
-        this.ctx.translate(-this.camera_x * 0.3, 0);
-        this.addObjectsToMap(this.level.middlegroundObjects);
-        this.ctx.translate(this.camera_x * 0.3, 0);
-    }
+flipImage(mo) {
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
+}
 
-    fixedObjects() {
-        this.ctx.translate(-this.camera_x, 0);
-        this.addObjectsToMap(this.bars);
-        this.ctx.translate(this.camera_x, 0);
-    }
+flipImageBack(mo) {
+    mo.x = mo.x * -1;
+}
 
-    frontObjects() {
-        this.addObjectsToMap(this.level.frontgroundObjects);
-        this.addObjectsToMap(this.level.enemies);
-        this.addObjectsToMap(this.throwableObjects);
-        this.addObjectsToMap(this.level.coins);
-        this.addObjectsToMap(this.level.poison);
-        this.addToMap(this.character);
-    }
-
-    addObjectsToMap(objects) {
-        objects.forEach(object => {
-            this.addToMap(object);
-        })
-    }
-
-    addToMap(mo) {
-        this.ctx.save();
-        if (mo.leftDirection) {
-            this.flipImage(mo);
-        }
-        if (mo.upperDirection) {
-            this.rotateUpwards(mo);
-        }
-        mo.draw(this.ctx, mo);
-        mo.drawBorder(this.ctx);
-        if (mo.leftDirection) {
-            this.flipImageBack(mo);
-        }
-        this.ctx.restore();
-    }
-
-    flipImage(mo) {
-        this.ctx.translate(mo.width, 0);
-        this.ctx.scale(-1, 1);
-        mo.x = mo.x * -1;
-    }
-
-    flipImageBack(mo) {
-        mo.x = mo.x * -1;
-    }
-
-    rotateUpwards(mo) {
-        this.ctx.translate(mo.x + mo.width / 2, mo.y + mo.height / 2);
-        this.ctx.rotate(-this.rotation * Math.PI / 180);
-        this.ctx.translate(-(mo.x + mo.width / 2), -(mo.y + mo.height / 2));
-    }
+rotateUpwards(mo) {
+    this.ctx.translate(mo.x + mo.width / 2, mo.y + mo.height / 2);
+    this.ctx.rotate(-this.rotation * Math.PI / 180);
+    this.ctx.translate(-(mo.x + mo.width / 2), -(mo.y + mo.height / 2));
+}
 }
