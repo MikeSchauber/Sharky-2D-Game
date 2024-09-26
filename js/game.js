a4b5024a3733cd184182aee7f64b8635b7e272f8
Mike Schauber <mike.schauber96@gmail.com>
2024-09-26 13:36:51 +0200

    fixed esc button bug in fullscreen mode

--- js/game.js
+++ js/game.js
@@ -13,309 +13,307 @@ let volume = true;
  * Initializes the application.
  */
 async function init() {
-    startTouchEventListener();
-    load();
+  startTouchEventListener();
+  load();
 }
 
 /**
  * Initializes the game.
- * 
+ *
  * @param {string} tryagain - The ID of the element to hide if the game is being retried.
  */
 async function initGame(tryagain) {
-    startLoadingscreen(tryagain);
-    hideMenuButtons();
-    canvas = document.getElementById('canvas');
-    clearAllIntervals();
-    await initLevel();
-    await startWorld();
-    checkLastVolume();
-    stopLoadingscreen();
+  startLoadingscreen(tryagain);
+  hideMenuButtons();
+  canvas = document.getElementById("canvas");
+  clearAllIntervals();
+  await initLevel();
+  await startWorld();
+  checkLastVolume();
+  stopLoadingscreen();
 }
 
 async function startWorld() {
-    world = new World(canvas, keyboard);
+  world = new World(canvas, keyboard);
 }
 
 /**
  * Starts the loading screen.
- * 
+ *
  * @param {string} tryagain - The ID of the element to hide if the game is being retried.
  */
 function startLoadingscreen(tryagain) {
-    if (tryagain == "firststart") {
-        document.getElementById('loadingScreen').style.display = "";
-        document.getElementById('loadingScreen').style.opacity = 1;
-    } else {
-        document.getElementById(tryagain).classList.add("d-none");
-    }
-
+  if (tryagain == "firststart") {
+    document.getElementById("loadingScreen").style.display = "";
+    document.getElementById("loadingScreen").style.opacity = 1;
+  } else {
+    document.getElementById(tryagain).classList.add("d-none");
+  }
 }
 
 /**
  * Stops the loading screen and starts the background music.
  */
 function stopLoadingscreen() {
-    setInterval(() => {
-        if (world.loaded) {
-            console.log("hello");
-            document.getElementById('loadingScreen').style.opacity = 0;
-            setTimeout(() => {
-                document.getElementById('loadingScreen').style.display = "none";
-            }, 150);
-        }
-    }, 1000);
-
+  setInterval(() => {
+    if (world.loaded) {
+      document.getElementById("loadingScreen").style.opacity = 0;
+      setTimeout(() => {
+        document.getElementById("loadingScreen").style.display = "none";
+      }, 150);
+    }
+  }, 1000);
 }
-
 
 /**
  * Hides menu buttons.
  */
 function hideMenuButtons() {
-    document.getElementById("impress").style.display = "none";
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('hud').style.display = '';
-    document.getElementById('sound').style.display = '';
-    document.querySelector("h1").style.display = 'none';
+  document.getElementById("impress").style.display = "none";
+  document.getElementById("start").style.display = "none";
+  document.getElementById("hud").style.display = "";
+  document.getElementById("sound").style.display = "";
+  document.querySelector("h1").style.display = "none";
 }
 
 /**
  * Clears all intervals.
  */
 function clearAllIntervals() {
-    intervalIds.forEach(id => clearInterval(id));
-    intervalIds = [];
-    timeoutIds.forEach(id => clearTimeout(id));
-    timeoutIds = [];
-    animationFrameIds.forEach(id => cancelAnimationFrame(id));
-    animationFrameIds = [];
+  intervalIds.forEach((id) => clearInterval(id));
+  intervalIds = [];
+  timeoutIds.forEach((id) => clearTimeout(id));
+  timeoutIds = [];
+  animationFrameIds.forEach((id) => cancelAnimationFrame(id));
+  animationFrameIds = [];
 }
-
 
 /**
  * Enters fullscreen mode for a given element.
- * 
+ *
  * @param {HTMLElement} screen - The element to display in fullscreen.
  */
-function enterFullscreen(screen, canvas) {
-    if (screen.requestFullscreen) {
-        screen.requestFullscreen();
-
-    } else if (screen.msRequestFullscreen) {
-        screen.msRequestFullscreen();
-    } else if (screen.webkitRequestFullscreen) {
-        screen.webkitRequestFullscreen();
-    }
-    canvas.style.width = "100vw"
-    canvas.style.height = "100vh"
+function enterFullscreen(screen) {
+  if (screen.requestFullscreen) {
+    screen.requestFullscreen();
+  } else if (screen.msRequestFullscreen) {
+    screen.msRequestFullscreen();
+  } else if (screen.webkitRequestFullscreen) {
+    screen.webkitRequestFullscreen();
+  }
 }
 
 /**
  * Exits fullscreen mode.
  */
-function exitFullscreen(screen, canvas) {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    }
-    canvas.style.width = ""
-    canvas.style.height = ""
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
 }
 
 /**
  * Toggles fullscreen mode.
  */
 function toggleFullscreen() {
-    let screen = document.getElementById("container");
-    let canvas = document.getElementById("canvas");
-    if (!fullscreen) {
-        enterFullscreen(screen, canvas);
-        document.getElementById("fullscreen").src = "icons/fullscreen-exit.png";
-        fullscreen = true;
-    } else if (fullscreen) {
-        exitFullscreen(screen, canvas);
-        document.getElementById("fullscreen").src = "icons/fullscreen.png";
-        fullscreen = false;
-    }
+  let screen = document.getElementById("container");
+  if (!fullscreen) {
+    enterFullscreen(screen);
+  } else if (fullscreen) {
+    exitFullscreen();
+  }
 }
+
+/**
+ * Detecting Fullscreen mode to change relevant Elements.
+ */
+document.addEventListener("fullscreenchange", function (e) {
+  if (document.fullscreenElement) {
+    document.getElementById("fullscreen").src = "icons/fullscreen-exit.png";
+    fullscreen = true;
+  } else {
+    document.getElementById("fullscreen").src = "icons/fullscreen.png";
+    fullscreen = false;
+  }
+});
 
 /**
  * Toggles the display of the info/help section.
  */
 function toggleInfo() {
-    if (info) {
-        document.getElementById("help").style.opacity = 0;
-        setTimeout(() => {
-            document.getElementById("help").style.display = "none";
-        }, 125);
-        info = false;
-    } else if (!info) {
-        document.getElementById("help").style.display = "";
-        setTimeout(() => {
-            document.getElementById("help").style.opacity = 1;
-        }, 125);
-        info = true;
-    }
+  if (info) {
+    document.getElementById("help").style.opacity = 0;
+    setTimeout(() => {
+      document.getElementById("help").style.display = "none";
+    }, 125);
+    info = false;
+  } else if (!info) {
+    document.getElementById("help").style.display = "";
+    setTimeout(() => {
+      document.getElementById("help").style.opacity = 1;
+    }, 125);
+    info = true;
+  }
 }
 
 function checkLastVolume() {
-    if (!volume) {
-        document.getElementById("sound").src = "icons/sound-off.png";
-        world.musicSettings.pauseBackgroundMusic();
-    } else if (volume) {
-        document.getElementById("sound").src = "icons/sound-on.png";
-        world.musicSettings.playBackgroundMusic();
-    }
+  if (!volume) {
+    document.getElementById("sound").src = "icons/sound-off.png";
+    world.musicSettings.pauseBackgroundMusic();
+  } else if (volume) {
+    document.getElementById("sound").src = "icons/sound-on.png";
+    world.musicSettings.playBackgroundMusic();
+  }
 }
 
 /**
  * Toggles the volume between on and off.
  */
 function toggleVolume() {
-    if (volume) {
-        document.getElementById("sound").src = "icons/sound-off.png";
-        world.musicSettings.pauseBackgroundMusic();
-        volume = false;
-    } else if (!volume) {
-        document.getElementById("sound").src = "icons/sound-on.png";
-        world.musicSettings.playBackgroundMusic();
-        volume = true;
-    }
-    save();
+  if (volume) {
+    document.getElementById("sound").src = "icons/sound-off.png";
+    world.musicSettings.pauseBackgroundMusic();
+    volume = false;
+  } else if (!volume) {
+    document.getElementById("sound").src = "icons/sound-on.png";
+    world.musicSettings.playBackgroundMusic();
+    volume = true;
+  }
+  save();
 }
 
 /**
  * Saves the current volume setting to the local storage.
  */
 function save() {
-    let volumeAsText = JSON.stringify(volume);
-    localStorage.setItem('volume', volumeAsText);
+  let volumeAsText = JSON.stringify(volume);
+  localStorage.setItem("volume", volumeAsText);
 }
 
 /**
  * Loads the volume setting from the local storage.
  */
 function load() {
-    let volumeAsText = localStorage.getItem('volume');
-    volume = JSON.parse(volumeAsText);
+  let volumeAsText = localStorage.getItem("volume");
+  volume = JSON.parse(volumeAsText);
 }
-
 
 /**
  * Displays the victory screen.
  */
 function victory() {
-    document.getElementById("victory").classList.remove("d-none");
+  document.getElementById("victory").classList.remove("d-none");
 }
 
 /**
  * Displays the game over screen.
  */
 function gameover() {
-    document.getElementById("gameover").classList.remove("d-none");
-    world.musicSettings.gameover_sound.play();
-    let gameoverSoundTo = setTimeout(() => {
-        world.musicSettings.gameover_sound.volume = 0;
-    }, 5000);
-    timeoutIds.push(gameoverSoundTo);
+  document.getElementById("gameover").classList.remove("d-none");
+  world.musicSettings.gameover_sound.play();
+  let gameoverSoundTo = setTimeout(() => {
+    world.musicSettings.gameover_sound.volume = 0;
+  }, 5000);
+  timeoutIds.push(gameoverSoundTo);
 }
 
-document.addEventListener('keydown', (e) => {
-    e.preventDefault();
-    if (e.keyCode === 39) {
-        keyboard.RIGHT = true;
-    }
-    if (e.keyCode === 37) {
-        keyboard.LEFT = true;
-    }
-    if (e.keyCode === 32) {
-        keyboard.SPACE = true;
-    }
-    if (e.keyCode === 81) {
-        keyboard.ONE = true;
-    }
-    if (e.keyCode === 87) {
-        keyboard.TWO = true;
-    }
-    if (e.keyCode === 69) {
-        keyboard.THREE = true;
-    }
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode === 39) {
+    keyboard.RIGHT = true;
+  }
+  if (e.keyCode === 37) {
+    keyboard.LEFT = true;
+  }
+  if (e.keyCode === 32) {
+    keyboard.SPACE = true;
+  }
+  if (e.keyCode === 81) {
+    keyboard.ONE = true;
+  }
+  if (e.keyCode === 87) {
+    keyboard.TWO = true;
+  }
+  if (e.keyCode === 69) {
+    keyboard.THREE = true;
+  }
+  if (e.keyCode === 27) {
+  }
 });
 
-document.addEventListener('keyup', (e) => {
-    if (e.keyCode === 39) {
-        keyboard.RIGHT = false;
-    }
-    if (e.keyCode === 37) {
-        keyboard.LEFT = false;
-    }
-    if (e.keyCode === 32) {
-        keyboard.SPACE = false;
-    }
-    if (e.keyCode === 81) {
-        keyboard.ONE = false;
-    }
-    if (e.keyCode === 87) {
-        keyboard.TWO = false;
-    }
-    if (e.keyCode === 69) {
-        keyboard.THREE = false;
-    }
+document.addEventListener("keyup", (e) => {
+  if (e.keyCode === 39) {
+    keyboard.RIGHT = false;
+  }
+  if (e.keyCode === 37) {
+    keyboard.LEFT = false;
+  }
+  if (e.keyCode === 32) {
+    keyboard.SPACE = false;
+  }
+  if (e.keyCode === 81) {
+    keyboard.ONE = false;
+  }
+  if (e.keyCode === 87) {
+    keyboard.TWO = false;
+  }
+  if (e.keyCode === 69) {
+    keyboard.THREE = false;
+  }
 });
 
 /**
  * Starts the touch event listeners for the on-screen controls.
  */
 function startTouchEventListener() {
-    document.getElementById("btnLeft").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.LEFT = true;
-    });
-    document.getElementById("btnRight").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.RIGHT = true;
-    });
-    document.getElementById("btnUp").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.SPACE = true;
-    });
-    document.getElementById("btnOne").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.ONE = true;
-    });
-    document.getElementById("btnTwo").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.TWO = true;
-    });
-    document.getElementById("btnThree").addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        keyboard.THREE = true;
-    });
+  document.getElementById("btnLeft").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.LEFT = true;
+  });
+  document.getElementById("btnRight").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.RIGHT = true;
+  });
+  document.getElementById("btnUp").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.SPACE = true;
+  });
+  document.getElementById("btnOne").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.ONE = true;
+  });
+  document.getElementById("btnTwo").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.TWO = true;
+  });
+  document.getElementById("btnThree").addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keyboard.THREE = true;
+  });
 
-    document.getElementById("btnLeft").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.LEFT = false;
-    });
-    document.getElementById("btnRight").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.RIGHT = false;
-    });
-    document.getElementById("btnUp").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.SPACE = false;
-    });
-    document.getElementById("btnOne").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.ONE = false;
-    });
-    document.getElementById("btnTwo").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.TWO = false;
-    });
-    document.getElementById("btnThree").addEventListener("touchend", (e) => {
-        e.preventDefault();
-        keyboard.THREE = false;
-    });
+  document.getElementById("btnLeft").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.LEFT = false;
+  });
+  document.getElementById("btnRight").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.RIGHT = false;
+  });
+  document.getElementById("btnUp").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.SPACE = false;
+  });
+  document.getElementById("btnOne").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.ONE = false;
+  });
+  document.getElementById("btnTwo").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.TWO = false;
+  });
+  document.getElementById("btnThree").addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keyboard.THREE = false;
+  });
 }
